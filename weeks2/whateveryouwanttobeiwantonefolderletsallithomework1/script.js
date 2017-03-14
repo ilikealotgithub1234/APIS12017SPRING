2a13c946a45acca486fb764716f297dd81776c51
ilikealotgithub1234 <erikpmurdock@gmail.com>
2017-03-14 12:28:10 -0400

    Added all classwork and some completed hw with a bug
    the generate score punction is not functional :palm_tree:

---
+++ weeks2/whateveryouwanttobeiwantonefolderletsallithomework1/script.js
@@ -0,0 +1,16 @@
+let body = {
+  name : 'joe',
+  calories: 2000000,
+  ingredients: [
+    "blood", "bone", "salt", "steak sauce", "thousand island sauce", "clothes"
+  ]
+}
+
+let pinkSlime = {
+  name : 'bob',
+  calories : 2000,
+  ingredients : [
+    "cheese", "pickles", "tomato", "special sauce", "mayo"
+  ]
+
+}
